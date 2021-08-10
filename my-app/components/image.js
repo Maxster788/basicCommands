e3a1a387791e0b6e89a5f07d8a25515ef0a49021
Maxwell Donovan <maxdonovan@Maxwells-MacBook-Pro.local>
2021-08-10 20:57:17 +1000

    cjuycjhfjfy

---
+++ my-app/components/image.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Main() {
+  return (
+    
+      
+      
+
+      <main className={styles.main}>
+        <div>LimitedContractWork
+            <img src="/vercel.svg"></img>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img className="border-radius: 16px"src="/Mordor.jpg"></img>
+        
+    
+
+        </div>
+      </main>
+
+    
+  )
+}
+
